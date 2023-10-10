03ad30d858b7da1a8d8bcd87e8b0b698d80831ef
Christoffer Naug <86500176+christoffernaug@users.noreply.github.com>
2023-10-10 10:36:04 +0200

    Remove UserReservations from ListingsController

--- WildStays/Controllers/ListingsController.cs
+++ WildStays/Controllers/ListingsController.cs
@@ -231,20 +231,5 @@ namespace WildStays.Controllers
 
             return RedirectToAction(nameof(Index));
         }
-
-        // GET: Listings/MyReservations
-        [Authorize]
-        public async Task<IActionResult> Reservation()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return RedirectToAction("Login", "Identity/Account");
-            }
-
-            var reservations = await _itemRepository.GetReservationByUserId(user.Id);
-            return View(reservations);
-        }
-
     }
 }
